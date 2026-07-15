f8ee16bce3a8e119da798acc35636dce1ebe8d5d
Palak Aaryan <palakaaryan011@gmail.com>
2026-07-15 17:04:16 +0530

    Add print statements and basic input handling

---
+++ Day-01-Print-Function/practice.py
@@ -0,0 +1,13 @@
+print("Hello, World!")
+print("Palak", 21, 50.4, sep='-')
+print("welcome to python", end='!')
+print("programming")
+x = "true"
+print(type(x))
+a = int(input())
+b = int(input())
+c= a+b
+print(c)
+a = True + 7
+print(a)
+print(type(a))     #here python takes boolean value as 1 and false as 0
