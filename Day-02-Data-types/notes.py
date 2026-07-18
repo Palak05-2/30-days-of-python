d8336faf0086282903d7840a56bb69dfb4921bed
Palak Aaryan <palakaaryan011@gmail.com>
2026-07-18 15:39:24 +0530

    Add example code snippets in notes.py
    Added commented code snippets demonstrating various Python operations, including arithmetic, logical operations, and user input for digit summation.

---
+++ Day-02-Data-types/notes.py
@@ -0,0 +1,18 @@
+# print (2 + 3)
+# print (5 // 8)
+# print (6 > 3)
+# print ( 8 or 9)
+# a = 4
+# a += 4
+# print(a)
+# print( "M" in "Palak")
+
+#printing the sum of digits of anumber from user input
+# n = int(input("Enter a number: ")) 
+# a = n % 10
+# n = (n//10)
+# b = n % 10
+# n = (n//10)
+# c = n % 10
+# print("the sum of digits is: ", a+b+c)
+#here we used operator for getting the sum that is we first put the number to modulus operator then we get a seperate number and after that we double dividethe number to get a number without the single number then we preceed likewise.
